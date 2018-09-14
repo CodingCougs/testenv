2e0023e9831c224406eb547dd36d757465fe7d0d
Unknown <ryanbreitenfeldt@gmail.com>
2018-09-13 21:58:23 -0700

    Updated the tasks

--- nametest.c
+++ nametest.c
@@ -5,7 +5,11 @@ int main(){
     printf("Hello\n");
     printf("Ryan Breitenfeldt\n");
     printf("Manny Bonilla\n");
-    //add your name here
+    
+    // create a print statement with your name;
+    // Use commit and create a comment
+    // push to repo
+    // wait for changes to master then pull and see the changes
 
     return 0;
 }

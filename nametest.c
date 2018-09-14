7e0d7aee903ca32dd55163c2ffcdd1cb160375dc
Mr_Motto <Mr_Motto@users.noreply.github.com>
2018-09-14 16:10:39 -0700

    Created Funct. to display names
    Funct. roleCall will display names of users to screeen

--- nametest.c
+++ nametest.c
@@ -1,13 +1,25 @@
 #include<stdio.h>
 
+/*@function roleCall prints students name in aphabetical order to screen when function is called. */
+void roleCall()
+{
+    printf("Names of students using Github and working on nametest.c:\n");
+    printf("Bobby Thompson\n");
+    printf("Chris Burry\n");   
+    printf("Manny Bonilla\n");
+	printf("Micheal Roper\n");
+    printf("Ryan Breitenfeldt\n");
+
+
+
+}
+
+
 int main(){
 
-    printf("Hello\n");
-    printf("Ryan Breitenfeldt\n");
-    printf("Manny Bonilla\n");
-	printf("Bobby Thompson\n");
-    printf("Micheal Roper\n");
-    printf("chris burry \n");
+// Add new student names to roleCall function.
+void roleCall();
+roleCall();
 
     // create a print statement with your name;
     // Use commit and create a comment

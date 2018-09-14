c9ad3bc5bbc4d473e346344678c1c76645871e9e
Bobby <32147830+BlueEyes360@users.noreply.github.com>
2018-09-14 16:07:06 -0700

    Added userInput and switch statement
    userInput is a single character from the user. It is the deciding character for the switch case statement to display the names or not.

--- nametest.c
+++ nametest.c
@@ -1,13 +1,31 @@
 #include<stdio.h>
 
-int main(){
+int main()
+{
+    char userInput;
+    
+    printf("Do you want to see the list of names? Enter y for yes and n for no: ");
+    
+    scanf("%c", &userInput);
 
-    printf("Hello\n");
-    printf("Ryan Breitenfeldt\n");
-    printf("Manny Bonilla\n");
-	printf("Bobby Thompson\n");
-    printf("Micheal Roper\n");
-    printf("chris burry \n");
+    switch (userInput)
+    {
+        case 'y':
+            printf("Hello\n");
+            printf("Ryan Breitenfeldt\n");
+            printf("Manny Bonilla\n");
+            printf("Bobby Thompson\n");
+            printf("Micheal Roper\n");
+            printf("chris burry\n");
+            break;
+        case 'n':
+            printf("Thats alright, I didn't want to tell you anyways!\n");
+            break;  
+        default:
+            printf("Your keyboard-fu needs some work. You entered something wrong.\n");
+            break;
+    }
+
 
     // create a print statement with your name;
     // Use commit and create a comment

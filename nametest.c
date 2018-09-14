7d0fdd6648e9e01859943bdd8ba249b5c1e4090c
Emmanuel Bonilla <emmanuel.bonilla@wsu.edu>
2018-09-14 16:17:50 -0700

    boby
    boby please fix your problem

--- nametest.c
+++ nametest.c
@@ -1,12 +1,5 @@
 #include<stdio.h>
-
-int main()
-{
-    char userInput;
-    
-    printf("Do you want to see the list of names? Enter y for yes and n for no: ");
-    
-    scanf("%c", &userInput);
+void roleCall();
 /*@function roleCall prints students name in aphabetical order to screen when function is called. */
 void roleCall()
 {
@@ -16,23 +9,17 @@ void roleCall()
     printf("Manny Bonilla\n");
 	printf("Micheal Roper\n");
     printf("Ryan Breitenfeldt\n");
-
-
-
 }
 
 
-int main(){
-
+int main()
+{
+    char userInput;
+    
     switch (userInput)
     {
         case 'y':
-            printf("Hello\n");
-            printf("Ryan Breitenfeldt\n");
-            printf("Manny Bonilla\n");
-            printf("Bobby Thompson\n");
-            printf("Micheal Roper\n");
-            printf("chris burry\n");
+            roleCall();
             break;
         case 'n':
             printf("Thats alright, I didn't want to tell you anyways!\n");
@@ -41,15 +28,4 @@ int main(){
             printf("Your keyboard-fu needs some work. You entered something wrong.\n");
             break;
     }
-// Add new student names to roleCall function.
-void roleCall();
-roleCall();
-
-
-    // create a print statement with your name;
-    // Use commit and create a comment
-    // push to repo
-    // wait for changes to master then pull and see the changes
-
-    return 0;
 }

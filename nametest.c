466cbde4250813af3d7527e89864e4f40ea90835
Emmanuel Bonilla <emmanuel.bonilla@wsu.edu>
2018-09-14 16:55:39 -0700

    new header
    created a new header for mike. Bobby all you have to do is make the fixes for main.

--- nametest.c
+++ nametest.c
@@ -1,21 +1,10 @@
 #include<stdio.h>
-void roleCall();
-/*@function roleCall prints students name in aphabetical order to screen when function is called. */
-void roleCall()
-{
-    printf("Names of students using Github and working on nametest.c:\n");
-    printf("Bobby Thompson\n");
-    printf("Chris Burry\n");   
-    printf("Manny Bonilla\n");
-	printf("Micheal Roper\n");
-    printf("Ryan Breitenfeldt\n");
-}
-
+#include "roleCall.h"
 
 int main()
 {
     char userInput;
-    
+
     switch (userInput)
     {
         case 'y':
---
+++ roleCall.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/*@function roleCall prints students name in aphabetical order to screen when function is called. */
+void roleCall()
+{
+    printf("Names of students using Github and working on nametest.c:\n");
+    printf("Bobby Thompson\n");
+    printf("Chris Burry\n");   
+    printf("Manny Bonilla\n");
+	printf("Micheal Roper\n");
+    printf("Ryan Breitenfeldt\n");
+}
